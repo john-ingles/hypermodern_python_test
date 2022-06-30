4369c7bfb31e48650833e2fb76214f4379d0fd57
John Ingles <john.kenneth.ingles@gmail.com>
2022-06-30 17:57:28 -0400

    Add flake8-safety to default nox session

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,9 @@
+import os
+import tempfile
+
 import nox
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 
 
 locations = "src", "tests", "noxfile.py"
@@ -31,3 +34,21 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.9")
+def safety(session):
+    requirements = tempfile.NamedTemporaryFile(mode="w", delete=False)
+    session.run(
+        "poetry",
+        "export",
+        "--dev",
+        "--format=requirements.txt",
+        "--without-hashes",
+        f"--output={requirements.name}",
+        external=True,
+    )
+    session.install("safety")
+    session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+    requirements.close()
+    os.remove(requirements.name)
